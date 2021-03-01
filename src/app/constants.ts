795169f15bc1126a9cfa431609b53774ec778c15
Michael Baghel <michael.baghel@gmail.com>
2021-03-01 18:17:00 +0400

    update for deploy

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -1,5 +1,5 @@
-export const PROXY_URL = 'https://thingproxy.freeboard.io/fetch/';
-export const BASE_URL = 'https://giantbomb.com';
+export const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
+export const BASE_URL = 'https://www.giantbomb.com';
 export const APP_NAME = 'randomizer';
 export const LOGIN_PATH = `/app/${APP_NAME}/get-result`;
 export const TOKEN_KEY = 'gb_test_token';
--- src/app/randomizer/randomizer.service.ts
+++ src/app/randomizer/randomizer.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { AuthService } from '../auth/auth.service';
+import { BASE_URL } from '../constants';
 
 @Injectable()
 export class RandomizerService {
@@ -11,7 +12,7 @@ export class RandomizerService {
 
   /** GET number of videos from server */
   getNumVideos(): Observable<number> {
-    const numVidsUrl = 'https://giantbomb.com/api/videos/?api_key=' + this.authService.getApiKey() + '&format=jsonp&limit=1&field_list=id';
+    const numVidsUrl = BASE_URL + '/api/videos/?api_key=' + this.authService.getApiKey() + '&format=jsonp&limit=1&field_list=id';
     return this.http.jsonp(numVidsUrl, 'json_callback')
       .pipe(map((data: any) => data.number_of_total_results));
   }
@@ -19,7 +20,7 @@ export class RandomizerService {
   /** GET a random video */
   getRandomVideo(videoCount: number): Observable<any> {
     const randOffset = Math.floor(Math.random() * videoCount);
-    const randVidUrl = 'https://giantbomb.com/api/videos/?api_key=' + this.authService.getApiKey() + '&format=jsonp&limit=1' +
+    const randVidUrl = BASE_URL + '/api/videos/?api_key=' + this.authService.getApiKey() + '&format=jsonp&limit=1' +
       '&offset=' + randOffset + '&field_list=deck,id,image,name,publish_date,site_detail_url,video_show';
     return this.http.jsonp(randVidUrl, 'json_callback')
       .pipe(map((data: any) => {
